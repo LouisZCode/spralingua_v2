5b17945c0b862c17cc3029dc0b32f2024e0ad644
Luis Zermeno <lgzg90@hotmail.com>
2026-01-02 20:53:59 +0100

    feature: initial api call with new key working

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from spralingua-v2!")
+from dotenv import load_dotenv
+from langchain.agents import create_agent
 
+load_dotenv()
 
-if __name__ == "__main__":
-    main()
+comedian_prompt = "You are a full stack comedian"
+
+agent = create_agent(
+    model="openai:gpt-5-mini",
+    system_prompt=comedian_prompt
+)
+
+message = "tell me a joke about mexico city"
+
+result = agent.invoke(
+    {"role": "user", "content": message}
+)
+
+for i, msg in enumerate(result["messages"]):
+    msg.pretty_print()
